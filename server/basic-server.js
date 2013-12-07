a1753787422de98267fd1a014f725b13ad6cbe79
Hack Reactor Students <lindseyengineer@gmail.com>
2013-12-07 11:44:21 -0800

    Dynamic port for the win

--- server/basic-server.js
+++ server/basic-server.js
@@ -7,19 +7,16 @@ var http = require("http");
  * standard port for HTTP servers is port 80, but that port is
  * normally already claimed by another server and/or not accessible
  * so we'll use a higher port number that is not likely to be taken: */
-var port = 5000;
-
 /* For now, since you're running this server on your local machine,
  * we'll have it listen on the IP address 127.0.0.1, which is a
  * special address that always refers to localhost. */
-var ip = "http://aqueous-island-6585.herokuapp.com/";
 
 /* We use node's http module to create a server. Note, we called it 'server', but
 we could have called it anything (myServer, blahblah, etc.). The function we pass it (handleRequest)
 will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in the 'request-handler' file).
 Lastly, we tell the server we made to listen on the given port and IP. */
 var server = http.createServer(request.handleRequest);
-server.listen(port);
+server.listen(process.env.PORT || 5000);
 
 /* To start this server, run:
      node basic-server.js
